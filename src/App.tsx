271dc39fbbfb95df45b938f1face1e6a65adb769
Mikolaj Olejnik <olejnikmikolaj@gmail.com>
2022-07-10 23:44:04 +0200

    added icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { ReactComponent as ReactLogo } from './shiba.svg';
-
+import Icons from './Icons';
 import './App.scss';
 
 function App() {
     return (
     <div className="App">
-        <h1>Hi! I'm Mikołaj and I like <span className='shiba'>Shiba Inu</span></h1>
+        <h1>Hi! I'm Mikołaj!</h1>
+        <div>
+            <h2>Full-stack developer</h2>
+            <div className='underline'></div>
+        </div>
+        <Icons />
         <div className='shiba-wrapper'>
             <ReactLogo onClick={() => {window.open('https://en.wikipedia.org/wiki/Shiba_Inu')}}/>
         </div>
+        <h3>Also, I love <span className='shiba'>Shiba Inu</span></h3>
     </div>
     );
 }
---
+++ src/Icons.tsx
@@ -0,0 +1,14 @@
+import { FaGithub, FaLinkedin, FaEnvelope, FaTwitter } from 'react-icons/fa';
+
+import './Icons.scss';
+
+export default function Icons () {
+    return (
+    <div className='icons'>
+        <FaGithub />
+        <FaLinkedin />
+        <FaEnvelope />
+        <FaTwitter />
+    </div>
+    );
+}
