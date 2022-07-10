c06f94c933f6e1a8dd479fe737fa30126e595829
Mikolaj Olejnik <olejnikmikolaj@gmail.com>
2022-07-10 16:56:11 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {ReactComponent as ReactLogo} from './shiba.svg';
+
+import './App.scss';
 
 function App() {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hi! I'm Mikołaj and I like Shibas</h1>
+        <div className='shiba-wrapper'>
+            <ReactLogo onClick={() => {window.open('https://en.wikipedia.org/wiki/Shiba_Inu')}}/>
+        </div>
     </div>
-  );
+    );
 }
 
 export default App;
