1388ced545dd8bb87ec2eda929ac9c5ae275c4a7
Mikołaj Olejnik <olejnikmikolaj@gmail.com>
2022-07-13 22:50:29 +0200

    fixed email

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -7,13 +7,13 @@ export default function Icons () {
         github: 'https://github.com/Mikolalaj',
         linkedin: 'https://www.linkedin.com/in/mikolalaj/',
         twitter: 'https://twitter.com/Mikolalaj_',
-        email: 'mailto:olejnikmikolaj' + '@gmail'
+        email: 'mailto:olejnikmikolaj@gmail.com'
     }
     return (
     <div className='icons'>
         <FaGithub onClick={() => {window.open(links.github)}} />
         <FaLinkedin onClick={() => {window.open(links.linkedin)}} />
-        <FaEnvelope onClick={() => {window.location.href = links.email + '.com'}} />
+        <FaEnvelope onClick={() => {window.location.href = links.email}} />
         <FaTwitter onClick={() => {window.open(links.twitter)}} />
     </div>
     );
